296fcbab5eec00f163f0abc15d6c58c74df73870
huangkaiqiao <huangkaiqiao@gmail.com>
2021-09-01 19:48:14 +0800

    crypt file

--- crypto/aes.go
+++ crypto/aes.go
@@ -4,9 +4,13 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
-	"encoding/hex"
 	"fmt"
 	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	// "math/rand"
 )
 
 type Cipher struct {
@@ -15,8 +19,14 @@ type Cipher struct {
 	nonce  []byte
 }
 
+func randomKey() []byte {
+	key := make([]byte, 32)
+	return key
+}
+
 func newCipher() (Cipher, error) {
-	key, _ := hex.DecodeString("6368616e676520746869732070617373776f726420746f206120736563726574")
+	// key, _ := hex.DecodeString("6368616e676520746869732070617373776f726420746f206120736563726574")
+	key := randomKey()
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		panic(err.Error())
@@ -45,9 +55,121 @@ func (c *Cipher) encrypt(plaintext []byte) ([]byte, error) {
 	// plaintext := []byte("exampleplaintext")
 	// plaintext := []byte(msg)
 
+	// nonce, _ := hex.DecodeString("64a9433eae7ccceee2fc0eda")
+
 	ciphertext := c.aesgcm.Seal(nil, c.nonce, plaintext, nil)
 	fmt.Printf("%x\n", ciphertext)
 	return ciphertext, nil
+}
+
+func (c *Cipher) encryptFile(inpath string) (string, error) {
+	infile, err := os.Open(inpath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer infile.Close()
+	fi, _ := infile.Stat()
+	size := fi.Size()
+	// Never use more than 2^32 random nonces with a given key
+	// because of the risk of repeat.
+	outfile, err := os.OpenFile(inpath+".bin", os.O_RDWR|os.O_CREATE, 0777)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer outfile.Close()
+	// The buffer size must be multiple of 16 bytes
+	buf := make([]byte, 1024)
+	for {
+		n := -1
+		var err error
+		if size < 1024 && 0 < size {
+			buf1 := make([]byte, size)
+			buf = make([]byte, 1024)
+			n, err = infile.Read(buf1)
+			copy(buf, buf1)
+			fmt.Println(size)
+		} else {
+			n, err = infile.Read(buf)
+		}
+		size = size - int64(n)
+		if n > 0 {
+			//stream.XORKeyStream(buf, buf[:n])
+			ciphertext := c.aesgcm.Seal(nil, c.nonce, buf, nil)
+			// Write into file
+			outfile.Write(ciphertext)
+		}
+
+		if err == io.EOF {
+			fmt.Println("EOF")
+			ciphertext := c.aesgcm.Seal(nil, c.nonce, buf, nil)
+			outfile.Write(ciphertext)
+			break
+		}
+
+		if err != nil {
+			log.Printf("Read %d bytes: %v", n, err)
+			break
+		}
+	}
+	// Append the IV
+	outfile.Write(c.nonce)
+	return outfile.Name(), nil
+}
+
+func (c *Cipher) decryptFile(inpath string) (string, error) {
+	infile, err := os.Open(inpath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer infile.Close()
+
+	// Never use more than 2^32 random nonces with a given key
+	// because of the risk of repeat.
+	fi, err := infile.Stat()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	iv := make([]byte, 12)
+	msgLen := fi.Size() - int64(len(iv))
+	_, err = infile.ReadAt(iv, msgLen)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	outpath := strings.TrimSuffix(inpath, filepath.Ext(inpath)) + ".out"
+	outfile, err := os.OpenFile(outpath, os.O_RDWR|os.O_CREATE, 0777)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer outfile.Close()
+
+	// The buffer size must be multiple of 16 bytes
+	buf := make([]byte, 1040)
+	for {
+		n, err := infile.Read(buf)
+		if n > 0 {
+			// The last bytes are the IV, don't belong the original message
+			if n > int(msgLen) {
+				n = int(msgLen)
+			}
+			msgLen -= int64(n)
+			// stream.XORKeyStream(buf, buf[:n])
+			plaintext, _ := c.aesgcm.Open(nil, c.nonce, buf, nil)
+			// Write into file
+			outfile.Write(plaintext)
+		}
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Printf("Read %d bytes: %v", n, err)
+			break
+		}
+	}
+	return outpath, nil
 }
 
 func (c *Cipher) decrypt(ciphertext []byte) ([]byte, error) {
--- crypto/aes_test.go
+++ crypto/aes_test.go
@@ -3,15 +3,32 @@ package crypto
 import (
 	// "crypto/aes"
 
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestAes(t *testing.T) {
-	plaintext := []byte("exampleplaintext")
+func TestAesGcmList(t *testing.T) {
+	testAesGcm(t, "exampleplaintext")
+	testAesGcm(t, "聊聊 Go 语言中的面向对象编程")
+	testAesGcm(t, "我看到很多成年人在评论区冷嘲热讽, 等你们未成年的时候看谁帮你们说话")
+}
+
+func testAesGcm(t *testing.T, msg string) {
+	plaintext := []byte(msg)
 	c, _ := newCipher()
 	ciphertext, _ := c.encrypt(plaintext)
 	decrypttext, _ := c.decrypt(ciphertext)
 	assert.Equal(t, plaintext, decrypttext)
+}
+
+func TestAesGcmFile(t *testing.T) {
+	c, _ := newCipher()
+	inpath := "plaintext.txt"
+	outpath, _ := c.encryptFile(inpath)
+	plainpath, _ := c.decryptFile(outpath)
+	fmt.Println(plainpath)
+	// TODO
+	// assert.Equal(t, inpath, plainpath)
 }
